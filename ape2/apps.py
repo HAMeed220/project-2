eb474214d909a26e27e246e838cf95d0a37f8f8d
HAMeed220 <singanamalahameed11@.com>
2023-11-08 22:20:27 +0530

    first commit

---
+++ ape2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Ape2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ape2'
---
+++ ape2/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django . http import HttpResponse
+
+
+# Create your views here.
+
+def silk(request):
+    return HttpResponse('we are not talking about dairy milk silk')
+
+
+
+
